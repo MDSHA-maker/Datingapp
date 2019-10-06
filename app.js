1e95246a6ef9d3646bf75a3b3fa33b0dc4bf5ab5
EC2 Default User <ec2-user@ip-172-31-9-125.us-east-2.compute.internal>
2019-10-06 22:34:13 +0000

    Pages routes done commit

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 var express =require("express")
 var app=express();
 var request=require("request");
+var bodyparser=require("body-parser");
+app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+ var user=[
+      
+      { name: "Monica" ,image:"https://previews.123rf.com/images/alfastudio/alfastudio1711/alfastudio171101661/89429393-hot-girl-trying-to-tear-a-t-shirt-on-a-gray-background.jpg"},
+    { name: "John", image: "https://i.pinimg.com/originals/16/66/cc/1666ccaa0140228d6f90c8c5ee8b378e.jpg"}                    
+         ];
 app.get("/",function(req,res){
     
    res.render("landing"); 
     
 });
 
+app.get("/userprofiles",function(req,res){
+  
+   
+   res.render("userprofiles",{user:user});
+   
+});
 
 app.listen(process.env.PORT,process.env.IP,function(){
    console.log("Dating server Started"); 
     
     
 });
+app.post("/userprofiles",function(req,res){
+   var name=req.body.name;
+   var image=req.body.image;
+   var newprofile={name:name, image:image }
+   user.push(newprofile);
+   //res.send("YOu hit post route");
+   res.redirect("/userprofiles");
+   
+});
+app.get("/userprofiles/new",function(req,res){
+   res.render("new.ejs");
+   
+   
+   
+})
