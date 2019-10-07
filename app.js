06ec0013f88e58bc9aca4ce630cf3297a0af9ed7
EC2 Default User <ec2-user@ip-172-31-9-125.us-east-2.compute.internal>
2019-10-07 03:54:06 +0000

    database functioning

--- app.js
+++ app.js
@@ -5,14 +5,47 @@ var mongoose = require('mongoose');
 var request=require("request");
 var bodyparser=require("body-parser");
 mongoose.connect("mongodb://localhost/dateapp")
+//Schema
+var UserSchema=new mongoose.Schema({
+   name: String,
+   image:String
+   
+   
+   
+});
+
+
+var users=mongoose.model("user",UserSchema);
+/*users.create({
+   
+     
+      name: "Monica" ,
+      image:"https://previews.123rf.com/images/alfastudio/alfastudio1711/alfastudio171101661/89429393-hot-girl-trying-to-tear-a-t-shirt-on-a-gray-background.jpg"},function(err,users)
+{
+   if(err){console.log("error");}
+     else {
+        console.log("Newly created user");
+     }
+});*/
+
+
+
+
+
+
+
+
+
+
+//---------------------------
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
- var user=[
+/* var user=[
       
       { name: "Monica" ,image:"https://previews.123rf.com/images/alfastudio/alfastudio1711/alfastudio171101661/89429393-hot-girl-trying-to-tear-a-t-shirt-on-a-gray-background.jpg"},
     { name: "John", image: "https://i.pinimg.com/originals/16/66/cc/1666ccaa0140228d6f90c8c5ee8b378e.jpg"}                    ,
          
-         {name:"Flora",image:"https://t3.ftcdn.net/jpg/02/14/52/34/500_F_214523426_9APpliHA9uj3EMel5aNUdDaHiXTjIVjH.jpg"}];
+         {name:"Flora",image:"https://t3.ftcdn.net/jpg/02/14/52/34/500_F_214523426_9APpliHA9uj3EMel5aNUdDaHiXTjIVjH.jpg"}];*/
 app.get("/",function(req,res){
     
    res.render("landing"); 
@@ -20,9 +53,21 @@ app.get("/",function(req,res){
 });
 
 app.get("/userprofiles",function(req,res){
-  
+  //GEt alll the users
+   users.find({},function(err,allusers){
+      if(err){
+         console.log(err);
+      }
+      else
+      {
+           res.render("userprofiles",{user:allusers});
+         
+      }
+      
+      
+   });
    
-   res.render("userprofiles",{user:user});
+ 
    
 });
 
@@ -34,10 +79,26 @@ app.listen(process.env.PORT,process.env.IP,function(){
 app.post("/userprofiles",function(req,res){
    var name=req.body.name;
    var image=req.body.image;
-   var newprofile={name:name, image:image }
-   user.push(newprofile);
+   var newprofile={name:name, image:image };
+  /// user.push(newprofile);
+   //create a new user
+   users.create(newprofile,function(err,newuser){
+      if(err)
+      {
+         console.log(err);
+         
+      }
+      
+      else
+      {
+        res.redirect("/userprofiles");
+      }
+      
+   });
+   
+   
    //res.send("YOu hit post route");
-   res.redirect("/userprofiles");
+ 
    
 });
 app.get("/userprofiles/new",function(req,res){
@@ -45,4 +106,4 @@ app.get("/userprofiles/new",function(req,res){
    
    
    
-})
+});
