d3ea746c7f03a692d53f717f60b64cac6fdc6710
EC2 Default User <ec2-user@ip-172-31-9-125.us-east-2.compute.internal>
2019-11-13 00:48:54 +0000

    deploying

--- chat.js
+++ chat.js
@@ -21,21 +21,21 @@ var conn = mysql.createConnection({
 
 
 
-conn.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-   var rm = "CREATE TABLE room (name VARCHAR(255) NOT NULL, user_1 VARCHAR(255) NOT NULL ,user_2 VARCHAR(255) NOT NULL,PRIMARY KEY(name))";
-   conn.query(rm, function (err, result) {
-     if (err) throw err;
-     console.log("room Table created");
-   });
-    var msg = "CREATE TABLE messages (name VARCHAR(255) NOT NULL , message VARCHAR(255) NOT NULL, handle VARCHAR(255)  NOT NULL, timestamp DATE NOT NULL, FOREIGN KEY (name) REFERENCES room(name))";
-  conn.query(msg, function (err, result) {
-    if (err) throw err;
-    console.log("Messages Table created");
-  });
+// conn.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var rm = "CREATE TABLE room (name VARCHAR(255) NOT NULL, user_1 VARCHAR(255) NOT NULL ,user_2 VARCHAR(255) NOT NULL,PRIMARY KEY(name))";
+//   conn.query(rm, function (err, result) {
+//      if (err) throw err;
+//      console.log("room Table created");
+//   });
+//     var msg = "CREATE TABLE messages (name VARCHAR(255) NOT NULL , message VARCHAR(255) NOT NULL, handle VARCHAR(255)  NOT NULL, timestamp DATE NOT NULL, FOREIGN KEY (name) REFERENCES room(name))";
+//   conn.query(msg, function (err, result) {
+//     if (err) throw err;
+//     console.log("Messages Table created");
+//   });
 
-});
+// });
 console.log(conn)
 
 
